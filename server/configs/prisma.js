88524424eb2b3865cc1fce1f4725a2ad21b1e59d
Divyanshu Raj <divyanshu.singh.1879@gmail.com>
2025-11-16 02:01:07 +0530

    solved server errorrrrrrr

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -4,21 +4,15 @@ import { PrismaNeon } from "@prisma/adapter-neon";
 import { neonConfig } from "@neondatabase/serverless";
 import ws from "ws";
 
-// WebSocket support for Neon
 neonConfig.webSocketConstructor = ws;
 
-// Let Neon parse the connection string internally
-neonConfig.connectionString = process.env.DATABASE_URL;
+const connectionString = `${process.env.DATABASE_URL}`;
 
-// Create adapter WITHOUT passing the URL
-const adapter = new PrismaNeon();
 
-// Normal Prisma client + adapter
-const prisma = global.prisma || new PrismaClient({
-  adapter
-});
+const adapter = new PrismaNeon({connectionString});
 
-// Keep prisma global in dev
+const prisma = global.prisma || new PrismaClient({ adapter });
+
 if (process.env.NODE_ENV === "development") {
   global.prisma = prisma;
 }
